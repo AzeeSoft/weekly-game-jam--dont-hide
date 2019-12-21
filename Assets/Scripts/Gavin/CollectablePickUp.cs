89962e89ffb7b07d6b569fc491c1c3280edb3d47
William Lacalle <digitalbros7@gmail.com>
2019-12-21 10:58:20 -0600

    Adds collectable prefab for testing (Destroys obj instead of deactivate)

---
+++ Assets/Scripts/Gavin/CollectablePickUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectablePickUp : MonoBehaviour
+{
+    public Interactable interactable;
+
+    void Update()
+    {
+        if (interactable.canPress && Input.GetKeyDown(KeyCode.E)) 
+        {
+            //Add name or just increment number of collectables in Game Manager.
+            Debug.Log(gameObject.name + " has been collected!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Gavin/Interactable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI textMesh;
+    public string text;
+
+    [HideInInspector] public bool canPress = false;
+
+    private void Awake()
+    {
+        textMesh.text = "";
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            textMesh.text = text;
+            canPress = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            textMesh.text = "";
+            canPress = false;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        textMesh.text = "";
+    }
+}
