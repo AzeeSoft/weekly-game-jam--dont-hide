0fb7d48acb02af91f6904dd8bc4dc47d73312e00
William Lacalle <digitalbros7@gmail.com>
2019-12-21 04:58:00 -0600

    Writes in the FOV for enemies and adds Standard Asset player

---
+++ Assets/Scripts/Gavin/Enemy AI/FieldOfView.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float rangeRadius;
+    [Range(0, 360)] public float viewAngle;
+
+    [HideInInspector] public LayerMask playerMask;
+    [HideInInspector] public LayerMask obstacleMask;
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    public Transform player;
+
+    void Awake()
+    {
+        playerMask = LayerMask.GetMask("Player");
+        obstacleMask = LayerMask.GetMask("Obstacle");
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, rangeRadius);
+
+        Vector3 viewAngleA = DirFromAngle(-viewAngle / 2, false);
+        Vector3 viewAngleB = DirFromAngle(viewAngle / 2, false);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + viewAngleA * rangeRadius);
+        Gizmos.DrawLine(transform.position, transform.position + viewAngleB * rangeRadius);
+
+        Gizmos.color = Color.red;
+        foreach (Transform visibleTarget in visibleTargets)
+        {
+            Gizmos.DrawLine(transform.position, visibleTarget.position);
+        }
+    }
+
+    public void FindPlayer()
+    {
+        visibleTargets.Clear();
+        Collider[] targetInRange = Physics.OverlapSphere(transform.position, rangeRadius, playerMask);
+
+        if (targetInRange.Length == 0)
+        {
+            return;
+        }
+        else
+        {
+            Transform target = targetInRange[0].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float disToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!(Physics.Raycast(transform.position, dirToTarget, disToTarget, obstacleMask)))
+                {
+                    //Switch to chase or shoot. 
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0.0f, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
