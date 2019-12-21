639743192cbab080cb92402f1cc8a83d5a996de1
Azee <aziztitu1996@gmail.com>
2019-12-21 11:44:17 -0600

    Implements health regeneration

---
+++ Assets/Scripts/Player/CamouflageModeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CamouflageModeController : MonoBehaviour
+{
+    public bool isCamouflaged { get; private set; } = false;
+    public float timeOfLastCamouflage { get; private set; } = 0;
+    public float timeSinceLastCamouflage => Time.time - timeOfLastCamouflage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateTimeOfLastCamouflage();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("CamouflageToggle"))
+        {
+            ToggleCamouflage();
+        }
+
+        if (isCamouflaged)
+        {
+            UpdateTimeOfLastCamouflage();
+        }
+    }
+
+    public void ToggleCamouflage()
+    {
+        isCamouflaged = !isCamouflaged;
+    }
+
+    private void UpdateTimeOfLastCamouflage()
+    {
+        timeOfLastCamouflage = Time.time;
+    }
+}
--- Assets/Scripts/Player/PlayerModel.cs
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class PlayerModel : MonoBehaviour
 {
+    public CamouflageModeController camouflageModeController { get; private set; }
     public Health health { get; private set; }
 
+    public float delayBeforeHealthRegeneration = 3f;
+    public float healthRegenerationSpeed = 1f;
     public Transform playerTarget;
 
     void Awake()
     {
+        camouflageModeController = GetComponent<CamouflageModeController>();
         health = GetComponent<Health>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (health.currentHealth < health.maxHealth &&
+            camouflageModeController.timeSinceLastCamouflage > delayBeforeHealthRegeneration)
+        {
+            health.UpdateHealth(healthRegenerationSpeed * Time.deltaTime);
+        }
     }
 }
