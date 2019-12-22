7004545f2253cad2d82e22a582c4fc866e6870c4
Azee <aziztitu1996@gmail.com>
2019-12-22 00:18:41 -0600

    Implements demons hiding and showing

--- Assets/Scripts/Demon/DemonModel.cs
+++ Assets/Scripts/Demon/DemonModel.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class DemonModel : MonoBehaviour
 {
+    public bool isVisible => avatar.activeSelf;
+
+    public GameObject avatar;
+
+    private PlayerModel playerModel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerModel = GameManager.Instance.playerModel;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerModel.isCamouflaged)
+        {
+            ShowDemon();
+        }
+        else
+        {
+            HideDemon();
+        }
+    }
+
+    private void ShowDemon()
+    {
+        if (!isVisible)
+        {
+            avatar.SetActive(true);
+        }
+    }
+
+    private void HideDemon()
+    {
+        if (isVisible)
+        {
+            avatar.SetActive(false);
+        }
     }
 }
