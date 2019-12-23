573d731663751aa0fd4368f0ff7cd2c4bdd917e1
William Lacalle <digitalbros7@gmail.com>
2019-12-23 14:44:22 -0600

    Adds music manager and implements functionality

---
+++ Assets/Scripts/Gavin/MusicManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : SingletonMonoBehaviour<MusicManager>
+{
+    public AudioClip basicClip;
+    public AudioClip advancedClip;
+
+    AudioSource audSource;
+
+    new void Awake()
+    {
+        base.Awake();
+
+        audSource = GetComponent<AudioSource>();
+        audSource.clip = basicClip;
+        audSource.Play();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (!GameManager.Instance.playerModel.isCamouflaged)
+            {
+                float timeToStart = audSource.time;
+                audSource.clip = advancedClip;
+                audSource.time = timeToStart;
+                audSource.Play();
+            }
+            else
+            {
+                float timeToStart = audSource.time;
+                audSource.clip = basicClip;
+                audSource.time = timeToStart;
+                audSource.Play();
+            }
+        }
+    }
+}
