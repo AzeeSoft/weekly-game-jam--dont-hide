5f2f458193a228202bdcd58bbcb84a0820ad1f51
Azee <aziztitu1996@gmail.com>
2019-12-26 12:52:15 -0600

    Implements Ending Sequence

---
+++ Assets/Scripts/Misc/EndingSequence.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingSequence : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnEndingSequenceEnded()
+    {
+        ScreenFader.Instance.FadeOutAndExecute(() => { SceneManager.LoadScene("MainMenu"); }, 5f);
+    }
+}
